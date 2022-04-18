6e9dc0ad2f30f24e29bd961a5cd373a55b78a4c1
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-18 21:07:38 +0530

    added TestCase1.2 for null check

--- src/test/java/com/measurement/QuantityMeasurementTest.java
+++ src/test/java/com/measurement/QuantityMeasurementTest.java
@@ -4,12 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import com.measurment.Feet;
 
-    /*
-	 * Testing class 
-	 */
+/*
+ * Testing class 
+ */
 public class QuantityMeasurementTest {
 	/*
-	 * UC1.1
+	 * Test Case UC 1.1
 	 */
 	@Test
 	public void givien0FeetAnd0Feet_ShouldReturnEqual() {
@@ -22,5 +22,35 @@ public class QuantityMeasurementTest {
 		 * checking by assert Equals method
 		 */
 		Assert.assertEquals(feet1, feet2);
+
 	}
+
+	/*
+	 * Checking for negative Test Case UC 1.1
+	 */
+	@Test
+	public void givien0FeetAnd1Feet_ShouldReturnNotEqual() {
+		Feet feet1 = new Feet(0.0);
+		Feet feet2 = new Feet(1.0);
+		/*
+		 * Checking by assert not Equals method
+		 */
+		Assert.assertNotEquals(feet1, feet2);
+
+	}
+
+	/*
+	 * Test Case UC1.2
+	 */
+	@Test
+	public void givenNullFeetValue_shouldReturnFalse() {
+		Feet feet = new Feet(0.0);
+
+		/*
+		 * checking by assert not null
+		 */
+		Assert.assertNotNull(feet);
+
+	}
+
 }
